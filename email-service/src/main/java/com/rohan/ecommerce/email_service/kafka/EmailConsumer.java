e9465f405424052d5139dacbf8cd47edd4727f89
RohanKotian <rohankotian9@gmail.com>
2025-12-21 14:19:41 +0530

    Add Email Service consuming order events

---
+++ email-service/src/main/java/com/rohan/ecommerce/email_service/kafka/EmailConsumer.java
@@ -0,0 +1,15 @@
+package com.rohan.ecommerce.email_service.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EmailConsumer {
+
+    @KafkaListener(topics = "order-topic", groupId = "email-group")
+    public void consume(Order order) {
+        log.info("Sending email for order: {}", order.getId());
+    }
+}
