7c6dd23fdb0d750e7bf3bbb05ee298278e704d61
RohanKotian <rohankotian9@gmail.com>
2026-03-24 21:38:02 +0530

    feat(api-gateway): add JWT utility for token parsing

---
+++ api-gateway/src/main/java/com/rohan/ecommerce/api_gateway/util/JwtUtil.java
@@ -0,0 +1,71 @@
+package com.rohan.ecommerce.api_gateway.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+
+        } catch (ExpiredJwtException e) {
+
+            throw new RuntimeException("JWT token has expired");
+
+        } catch (MalformedJwtException e) {
+
+            throw new RuntimeException("JWT token is malformed");
+
+        } catch (SignatureException e) {
+
+            throw new RuntimeException("JWT signature is invalid");
+
+        } catch (Exception e) {
+
+            throw new RuntimeException(
+                    "JWT token validation failed: " + e.getMessage()
+            );
+
+        }
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.get("role", String.class);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
