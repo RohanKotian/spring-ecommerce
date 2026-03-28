bd4ceae0fd146f5a7078bfc38777f270efaae552
RohanKotian <rohankotian9@gmail.com>
2026-03-28 23:13:00 +0530

    feat(order): add ProductResponse DTO mirroring product-service

---
+++ order-service/src/main/java/com/rohan/ecommerce/order_service/dto/ProductResponse.java
@@ -0,0 +1,27 @@
+package com.rohan.ecommerce.order_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private int stockQuantity;
+
+    private String category;
+
+    private boolean available;
+}
